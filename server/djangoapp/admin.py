7956eeb9c8f8c2dc5bb8a900ad9c6e928bf7affa
Abhilasha-R-Mishra <abhilasharmishra18@gmail.com>
2025-07-15 10:44:39 -0400

    Completed task create CarMake, CarModel Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,9 +3,14 @@
 
 
 # Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
 
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
