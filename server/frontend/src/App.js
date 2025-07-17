1bac4969ce4888ad065dd89d1691bed2606785f9
Abhilasha-R-Mishra <abhilasharmishra18@gmail.com>
2025-07-17 10:24:02 -0400

    Add dynamic pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register"
+import Dealers from './components/Dealers/Dealers';
 
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/Register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
