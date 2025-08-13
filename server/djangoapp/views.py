82029c681b241e1d93e0dbc924186425eeff573c
Abhilasha-R-Mishra <abhilasharmishra18@gmail.com>
2025-08-13 15:28:36 -0400

    Added new files in directory

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -113,6 +113,8 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
+    print("dealerships======")
+
     return JsonResponse({"status":200,"dealers":dealerships})
 # ...
 
